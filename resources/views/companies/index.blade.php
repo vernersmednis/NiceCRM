527e7a1e2db4eac11b599440dfdbd49b795459f4
Verners Mednis <verners.mednis@inbox.lv>
2024-09-05 16:32:59 +0300

    In companies.index view the 'Edit' and 'Delete' buttons have multiple language support

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -44,8 +44,8 @@
                                                 <td class="px-4 py-2 border">{{ $company->email }}</td> <!-- Display company email -->
                                                 <!-- "Edit" and "Delete" buttons -->
                                                 <td class="px-4 py-2 border">
-                                                    <button class="bg-white border border-blue-500 text-blue-500 px-4 py-2 rounded hover:bg-blue-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Edit</button>
-                                                    <button class="bg-orange-500 text-white px-4 py-2 rounded hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-opacity-50">Delete</button>
+                                                    <button class="bg-white border border-blue-500 text-blue-500 px-4 py-2 rounded hover:bg-blue-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">{{ __('Edit') }}</button>
+                                                    <button class="bg-orange-500 text-white px-4 py-2 rounded hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-opacity-50">{{ __('Delete') }}</button>
                                                 </td>
                                             </tr>
                                         @endforeach
