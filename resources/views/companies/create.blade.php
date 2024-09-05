a4ef531ec50022778b01585868f114ad44ecd912
Verners Mednis <verners.mednis@inbox.lv>
2024-09-05 16:16:05 +0300

    Update company.create view
    There is now content in the company.create view. The user can upload a company logo, set its name and email. He can't yet use the 'Create' button.

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -2,7 +2,7 @@
     <!-- Slot for the page header -->
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Edit Company') }}
+            {{ __('Create Company') }}
         </h2>
     </x-slot>
 
@@ -11,7 +11,42 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __('This page is currently in developement') }}
+                    <div>
+                        <div class="grid grid-cols-1 gap-0">
+                            <!-- Table title -->
+                            <div class="col-span-1 border px-8 py-4 text-xl">
+                                {{ __('Company Settings') }}
+                            </div>
+                            <!-- Table to display the company settings -->
+                            <div class="col-span-1 border p-8">
+                                <table class="min-w-max w-full bg-white border border-gray-300">
+                                    <thead>
+                                        <tr>
+                                            <!-- Table headers -->
+                                            <th class="px-4 py-2 border">{{ __('Logo') }}</th>
+                                            <th class="px-4 py-2 border">{{ __('Name') }}</th>
+                                            <th class="px-4 py-2 border">{{ __('Email') }}</th>
+                                            <th class="px-4 py-2 border"></th> <!-- Empty header for action buttons -->
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td class="px-4 py-2 border flex gap-4">
+                                                <img id="logoImage" src="" alt="Company Logo" class="w-10 h-10 object-cover">
+                                                <input id="logoInput" class="w-full" type="file" name="logo"/>
+                                            </td> <!-- Company logo -->
+                                            <td class="px-4 py-2 border"><input class="w-full" type="text" name="name" value=""/></td> <!-- Company name -->
+                                            <td class="px-4 py-2 border"><input class="w-full" type="text" name="email" value=""/></td> <!-- Company email -->
+                                            <!-- "Save" button -->
+                                            <td class="px-4 py-2 border">
+                                                <button type="submit" class="bg-white border border-blue-500 text-blue-500 px-4 py-2 rounded hover:bg-blue-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">{{ __('Create') }}</button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
