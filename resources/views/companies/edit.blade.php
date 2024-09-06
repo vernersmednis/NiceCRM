5daed0bb2fabd1bf9550d5edde4f5d79a26f53b1
Verners Mednis <verners.mednis@inbox.lv>
2024-09-06 18:51:29 +0300

    Unrefactor from components
    It turned out not componetizing the company index, create, edit was cleaner, so I reverted

--- resources/views/companies/edit.blade.php
+++ resources/views/companies/edit.blade.php
@@ -23,7 +23,33 @@
                                     @csrf
                                     @method('PUT')
                                     <!-- Table to display the company settings -->
-                                    <x-content-table :company="$company"/>
+                                    <table id="companies-table" class="display" data-storage="{{ asset('storage/') }}" data-ajax="">
+                                        <thead>
+                                            <tr>
+                                                <th>Logo</th>
+                                                <th>Name</th>
+                                                <th>Email</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <!-- Company logo -->
+                                                <td class="px-4 py-2 border flex gap-4">
+                                                    <img id="logoImage" src="{{ asset('storage/' . $company->logo) }}" alt="Company Logo" class="w-10 h-10 object-cover">
+                                                    <input id="logoInput" class="w-full" type="file" name="logo"/>
+                                                </td> 
+                                                <!-- Company name -->
+                                                <td class="px-4 py-2 border"><input class="w-full" type="text" name="name" value="{{ $company->name }}"/></td> 
+                                                <!-- Company email -->
+                                                <td class="px-4 py-2 border"><input class="w-full" type="text" name="email" value="{{ $company->email }}"/></td>
+                                                <!-- "Save" button -->
+                                                <td class="px-4 py-2 border">
+                                                    <button type="submit" class="bg-white border border-blue-500 text-blue-500 px-4 py-2 rounded hover:bg-blue-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">{{ __('Save') }}</button>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
                                     <!-- Error messages -->
                                     <div class="text-red-500">
                                         @error('logo'){{ $message }}<br>@enderror
